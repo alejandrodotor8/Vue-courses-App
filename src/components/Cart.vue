c64c6730fc6154cd180306cefc184a549a654ed5
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-04-19 23:00:27 -0500

    Start cart design

---
+++ src/components/Cart.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="m-4 shadow-xl rounded-lg w-60 h-80">
+		<img src="@/assets/img/course_1.jpg" alt="img" class="w-60 rounded-t-lg" />
+		<span class="text-base font-semibold m-4">Web Development</span>
+	</div>
+</template>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -14,4 +14,10 @@
 			</div>
 		</div>
 	</div>
+	<div>
+		<Cart />
+	</div>
 </template>
+<script setup>
+	import Cart from '@/components/Cart.vue'
+</script>
