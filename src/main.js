c7aad15d8d04297ea1d51796882ff7349bec0f7d
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-04-23 18:08:12 -0500

    init store

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 //Tailwind
 import './index.css'
 
-createApp(App)
+const app = createApp(App)
+app
 	.use(router)
+	.use(store)
 	.mount('#app')
