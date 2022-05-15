620ce32dab38c88b5bfb97a0cdfe97a738ae9390
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-05-15 12:16:45 -0500

    new images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,17 @@
 <template>
-	<main><h1>Hello Home.vue</h1></main>
+	<main>
+		<section class="">
+			<img src="/img/home/enjoy.png" alt="enjoy learning image" />
+			<div>
+				<h1>Make your learning Enjoyable</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac elementum tellus aliquet maecenas arcu
+					adipiscing nulla vel. Tellus tempus arcu sociis dignissim condimentum sem proin duis vivamus. Egestas amet,
+					nec amet ipsum.
+				</p>
+				<button>Get started</button>
+				<button>Watch video</button>
+			</div>
+		</section>
+	</main>
 </template>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,9 +2,9 @@
 	<main class="flex justify-center items-center pt-20">
 		<img src="/img/404.png" alt="404" style="width: 500px" />
 		<div class="pb-24 mx-14">
-			<h3 class="text-2xl font-light">Something went</h3>
-			<h4 class="text-5xl font-black">WRONG!</h4>
-			<router-link to="/" class="text-vue-300 font-semibold">Go Home</router-link>
+			<h3 class="text-5xl font-light">Something went</h3>
+			<h4 class="text-7xl font-black">WRONG!</h4>
+			<router-link to="/" class="text-xl text-vue-300 font-semibold">Go Home</router-link>
 		</div>
 	</main>
 </template>
