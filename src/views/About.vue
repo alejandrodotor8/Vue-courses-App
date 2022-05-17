65cce69a996a3723393a67b832568b9ca03149ce
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-05-17 14:48:26 -0500

    update images

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,10 @@
 <template>
-	<main><h1>This is about page</h1></main>
+	<main class="flex justify-center items-center pt-20 flex-col">
+		<img src="/img/about.png" alt="about" style="width: 400px" />
+		<div class="p-10 mx-14">
+			<h3 class="text-3xl font-bold">I built this website to learn</h3>
+			<h4 class="text-4xl font-thin my-2">Vue 3, Vuex, Vue Router, Vite and Tailwind.</h4>
+			<router-link to="/" class="text-xl text-vue-300 font-semibold">Go Home</router-link>
+		</div>
+	</main>
 </template>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,4 +8,3 @@
 		</div>
 	</main>
 </template>
-<style scope></style>
